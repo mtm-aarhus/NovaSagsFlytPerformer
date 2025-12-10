c76867ca98e3408e8d8f16f87a04d92a73a16cc7
Laura-Salmonsen <balas@aarhus.dk>
2025-12-10 16:19:30 +0100

    with correct config

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -9,17 +9,17 @@ FAIL_ROBOT_ON_TOO_MANY_ERRORS = True
 # Error screenshot config
 SMTP_SERVER = "smtp.adm.aarhuskommune.dk"
 SMTP_PORT = 25
-SCREENSHOT_SENDER = "robot@friend.dk"
+SCREENSHOT_SENDER = "novasagsflyt@aarhus.dk"
 
 # Constant/Credential names
-ERROR_EMAIL = "Error Email"
+ERROR_EMAIL = "balas"
 
 
 # Queue specific configs
 # ----------------------
 
 # The name of the job queue (if any)
-QUEUE_NAME = None
+QUEUE_NAME = 'NovaSagsFlyt'
 
 # The limit on how many queue elements to process
 MAX_TASK_COUNT = 100
